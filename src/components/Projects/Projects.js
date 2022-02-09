7b24edc351813a6a5a8af2ecdc343b46917b7f6a
Luiz Felipe <lfsprj@gmail.com>
2022-02-09 18:58:50 -0300

    Updated Projects and favicon.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,8 +30,8 @@ const Projects = () => (
               </TagList>
             </div>
             <UtilityList>
-              <ExternalLinks href={visit}>Code</ExternalLinks>
-              <ExternalLinks href={source}>Source</ExternalLinks>
+              <ExternalLinks href={visit}>Demo</ExternalLinks>
+              <ExternalLinks href={source}>Código</ExternalLinks>
 
             </UtilityList>
           </BlogCard>
