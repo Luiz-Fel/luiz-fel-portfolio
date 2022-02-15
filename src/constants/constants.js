46a45b05be9803304fcd8834e44579e332041ab8
Luiz Felipe <lfsprj@gmail.com>
2022-02-14 22:35:46 -0300

    Reordered projects.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,14 +1,5 @@
 export const projects = [
   {
-    title: 'dt money',
-    description: "Projeto em que construo uma dashboard de uma aplicação para controle financeiro.",
-      image: '/images/dtmoneyPic.png',
-      tags: ['React'],
-    source: 'https://github.com/Luiz-Fel/dtmoney',
-    visit: 'https://dtmoney-ivory.vercel.app/',
-    id: 0,
-  },
-  {
     title: 'CreamIce',
     description: "Neste projeto crio um site fictício de uma sorveteria utilizando apenas html, css e javascript.",
     image: '/images/CreamIceLogo.png',
@@ -20,11 +11,20 @@ export const projects = [
   {
     title: 'ig.news',
     description: "Um newsletter fictício de assuntos relacionados a react utilizando login no github.",
-      image: '/images/ignewsLogo.png',
-      tags: ['React', 'NextJS', 'FaunaDb', 'Stripe'],
+    image: '/images/ignewsLogo.png',
+    tags: ['React', 'NextJS', 'FaunaDb', 'Stripe'],
     source: 'https://github.com/Luiz-Fel/ignews',
     visit: 'https://vercel.com/luiz-fel/ignews',
     id: 2,
+  },
+  {
+    title: 'dt money',
+    description: "Projeto em que construo uma dashboard de uma aplicação para controle financeiro.",
+      image: '/images/dtmoneyPic.png',
+      tags: ['React'],
+    source: 'https://github.com/Luiz-Fel/dtmoney',
+    visit: 'https://dtmoney-ivory.vercel.app/',
+    id: 0,
   },
 ];
 
