9a3765168d47363faebb8800a2a23d9e6ce404c1
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-23 19:00:14 -0300

    Created handleSubmit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import form from "../form";
 
 const Contact = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+    };
+    console.log(data);
+    form.post('', data, {
+      headers: {
+        'Accept': 'application/json',
+    }})
+    .then(res => {
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  };
+
   return (
     <div
       name="contact"
@@ -15,7 +41,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            onSubmit={handleSubmit}
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
@@ -23,18 +49,21 @@ const Contact = () => {
               type="text"
               name="name"
               placeholder="Enter your name"
+              onChange={(e) => setName(e.target.value)}
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
               placeholder="Enter your email"
+              onChange={((e) => setEmail(e.target.value))}
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
+              onChange={((e) => setMessage(e.target.value))}
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
