1e71ee60ad5fb66ba4aaf97201b7fd29af1639ad
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2024-04-20 12:59:07 -0300

    Updated styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,21 +50,21 @@ const Contact = () => {
               name="name"
               placeholder="Enter your name"
               onChange={(e) => setName(e.target.value)}
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="p-2 bg-transparent border-2 border-gray-500 focus:border-white rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
               placeholder="Enter your email"
               onChange={((e) => setEmail(e.target.value))}
-              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="my-4 p-2 bg-transparent border-2 border-gray-500 focus:border-white rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
               onChange={((e) => setMessage(e.target.value))}
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="p-2 bg-transparent border-2 border-gray-500 focus:border-white rounded-md text-white focus:outline-none"
             ></textarea>
 
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
