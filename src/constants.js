089cf1626b980c5accac9e05faa5d1dfc6cd7f5b
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2024-04-26 21:17:50 -0300

    Revert "refactor: Separate constants"
    This reverts commit 89e48ca92218a2dc2b1be70119cf0ebc49002e80.

--- src/constants.js
+++ src/constants.js
@@ -41,118 +41,41 @@ export const links = [
 ];
 
 export const portfolios = [
-  {
-    id: 0,
-    src: shareNow,
-    title: "Sharenow",
-    description: "Image-based social media app",
-    demo: "https://sharenow-luiz-fel.netlify.app/login",
-    code: "https://github.com/Luiz-Fel/sharenow",
-    technologies: ["React.js", "Tailwind CSS", "Sanity.io"],
-  },
-  {
-    id: 1,
-    src: ignews,
-    title: "Ignews",
-    description:
-      "A newsletter on react-related topics with a subscription method and social media login",
-    demo: "https://ignews-luiz-fel.vercel.app/",
-    code: "https://github.com/Luiz-Fel/ignews",
-    technologies: ["Next.js", "Typescript", "Stripe"],
-  },
-  {
-    id: 2,
-    src: creamIce,
-    title: "Cream Ice",
-    description:
-      "A vanilla responsive javascript website for an ice cream parlor",
-    demo: "https://luiz-fel.github.io/CreamIce/",
-    code: "https://github.com/Luiz-Fel/CreamIce",
-    technologies: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    id: 3,
-    src: shopnow,
-    title: "ShopNow",
-    description: "An e-commerce website front page.",
-    demo: null,
-    code: "https://github.com/Luiz-Fel/Shopnow",
-    technologies: ["React.js", "Next.js", "Firebase"],
-  },
-];
-
-export const works = [
-  {
-    id: 0,
-    title: "Front-end Developer",
-    company: "Nova Tendência",
-    start: "May 2023",
-    startYear: "2023",
-    endYear: "Present",
-    end: "Present",
-    stacks: "React, Redux, TypeScript, MUI, Jest",
-  },
-  {
-    id: 1,
-    title: "Front-end Developer",
-    company: "Softeam",
-    start: "September 2021",
-    endYear: "2023",
-    startYear: "2021",
-    end: "May 2023",
-    stacks: "React, TypeScript, Tailwind, Figma",
-  },
-];
-export const techs = [
-  {
-    id: 4,
-    src: reactImage,
-    title: "React",
-    style: "shadow-blue-400",
-  },
-  {
-    id: 3,
-    src: javascript,
-    title: "JavaScript",
-    style: "shadow-yellow-500",
-  },
-  {
-    id: 5,
-    src: tailwind,
-    title: "Tailwind CSS",
-    style: "shadow-sky-400",
-  },
-  {
-    id: 6,
-    src: mui,
-    title: "Material UI",
-    style: "shadow-blue-600",
-    background: "",
-  },
-  {
-    id: 9,
-    src: figma,
-    title: "Figma",
-    style: "shadow-orange-500",
-    background: "bg-white",
-  },
-
-  {
-    id: 11,
-    src: typescript,
-    title: "TypeScript",
-    style: "shadow-blue-500",
-  },
-  {
-    id: 1,
-    src: redux,
-    title: "Redux",
-    style: "shadow-purple-500",
-  },
-  {
-    id: 10,
-    src: jest,
-    title: "Jest",
-    style: "shadow-red-500",
-  },
-];
+    {
+      id: 0,
+      src: shareNow,
+      title: "Sharenow",
+      description: "Image-based social media app",
+      demo: "https://sharenow-luiz-fel.netlify.app/login",
+      code: "https://github.com/Luiz-Fel/sharenow",
+      technologies: ["React.js", "Tailwind CSS", "Sanity.io"],
+    },
+    {
+      id: 1,
+      src: ignews,
+      title: "Ignews",
+      description:
+        "A newsletter on react-related topics with a subscription method and social media login",
+      demo: "https://ignews-luiz-fel.vercel.app/",
+      code: "https://github.com/Luiz-Fel/ignews",
+      technologies: ["Next.js", "Typescript", "Stripe"],
+    },
+    {
+      id: 2,
+      src: creamIce,
+      title: "Cream Ice",
+      description: "A vanilla responsive javascript website for an ice cream parlor",
+      demo: "https://luiz-fel.github.io/CreamIce/",
+      code: "https://github.com/Luiz-Fel/CreamIce",
+      technologies: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      id: 3,
+      src: shopnow,
+      title: "ShopNow",
+      description: "An e-commerce website front page.",
+      demo: null,
+      code: "https://github.com/Luiz-Fel/Shopnow",
+      technologies: ["React.js", "Next.js", "Firebase"],
+    },
+  ];
